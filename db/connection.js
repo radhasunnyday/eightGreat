5f5b283f807054256b8da45a4874a71ec90e8ed0
Christine Movius <christine.movius@gmail.com>
2016-04-30 11:02:12 -0400

    Define compliment schema.

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var ComplimentSchema = new mongoose.Schema(
+  {
+    body: String,
+    url: String,
+    tags: [String]
+  }
+);
+
+mongoose.model("Compliment", ComplimentSchema);
+
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGODB_URL);
+}else{
+  mongoose.connect("mongodb://localhost/eightgreat");
+}
+
+module.exports = mongoose;
