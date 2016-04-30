2f86d1b0e09977b9e28d5c207776fb9b4650a250
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-04-30 11:57:21 -0400

    index.js added

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var mongoose = require("./db/connection");
+var hbs = require("hbs");
+var parser =require("body-parser");
+var app = express();
+
+app.set("port", process.env.PORT || 3001);
+
+
+app.set("view engine", "hbs"):
+app.engine("hbs", hbs({
+  extname:".hbs"
+  partialsDir: "views/"
+  layoutsDir: "views/"
+  defaultLayout: "layout-main"
+}));
+
+app.use("/public", express.static("public"));
+app.get("/", function (req, res){
+  res.send("Hi!"):
+  res.render("compliments");
+});
+
+app.listen(app.get("port"), function(){
+  console.log("I'm woking!");
+});
